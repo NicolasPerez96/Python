1cad186b3c76ea91ec7e2ba352e8cd94ba72c608
Nicolas Perez <nicolasperezc96@gmail.com>
2025-12-01 15:27:09 -0300

    Agregado el modulo de Enemigo, con sus estadisticas y defensa, ademas de crear las funciones de atacar y recibir daño

--- Juego/Enemigo.py
+++ Juego/Enemigo.py
@@ -1,37 +1,48 @@
 class enemigoGenerico:
-    def __init__(self, daño, defensa, vida):
+    def __init__(self, nombre, daño, defensa, vida):
+        self.nombre = nombre
         self.daño = daño
         self.defensa = defensa
         self.vida = vida
+        self.vida_actual = self.vida
+        self.vida_restante = self.vida_actual
+        
+    def recibirDaño (self, dañoRecibido):
+        self.vida_actual -= (dañoRecibido - self.defensa)
+        return print(f"{self.nombre} recibio {dañoRecibido} le queda {self.vida_actual} de vida.") 
     
+    def atacar(self):
+        return self.daño   
 
-class goblin:
+class goblin(enemigoGenerico):
     def __init__(self):        
         
         super().__init__(
+            nombre = "Goblin" 
             daño = 15,
             defensa = 5,
-            vida = 50
-        )    
+            vida = 50            
+        )
+         
         
         
-        
-class lobo:
+class lobo(enemigoGenerico):
     def __init__(self):
         
         super().__init__(
+            nombre = "Lobo"
             daño = 25,
             defensa = 8,
             vida = 75
         )
-        
 
-class troll:
+class troll(enemigoGenerico):
     def __init__(self):
         
         super().__init__(
+            nombre = "Troll"
             daño = 40,
             defensa = 15,
             vida = 150
-        )
+        )     
         
